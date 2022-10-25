8572226260da77816244723b7042c3be3967f074
24-LowRongJun <damienlowrj2003@gmail.com>
2022-10-25 10:22:02 +0800

    fixed player jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,23 @@ public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
 
+    public float velocity = 2.4f;
+    private Rigidbody rigidbody;
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rigidbody.velocity = Vector2.up * velocity;
+        }
+            
     }
 }
